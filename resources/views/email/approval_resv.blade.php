184c548a7ef8a3ca0c150edc1a5cbe2a18fa622a
Wayan Tisna <wayantisna74@gmail.com>
2021-08-09 11:17:16 +0800

    update email

--- resources/views/email/approval_resv.blade.php
+++ resources/views/email/approval_resv.blade.php
@@ -8,15 +8,15 @@
         <th style="border: 1px solid #5e5b5b;text-align: left;padding: 4px;">Req Date</th>
         <th style="border: 1px solid #5e5b5b;text-align: left;padding: 4px;">Notes</th>
     </tr>
-    <tr>
-        @foreach ($details as $key => $value)
-            <td style="border: 1px solid #5e5b5b;text-align: left;padding: 4px;">{{ $value["ItemCode"] }} </td>
-            <td style="border: 1px solid #5e5b5b;text-align: left;padding: 4px;">{{ $value["ItemName"] }} </td>
-            <td style="border: 1px solid #5e5b5b;text-align: left;padding: 4px;">{{ $value["ItemCategory"] }} </td>
-            <td style="border: 1px solid #5e5b5b;text-align: left;padding: 4px;">{{ $value["UoMCode"] }} </td>
-            <td style="border: 1px solid #5e5b5b;text-align: left;padding: 4px;">{{ $value["ReqQty"] }} </td>
-            <td style="border: 1px solid #5e5b5b;text-align: left;padding: 4px;">{{ $value["ReqDate"] }} </td>
-            <td style="border: 1px solid #5e5b5b;text-align: left;padding: 4px;">{{ (array_key_exists('ReqNotes', $value)) ? $value["ReqNotes"] : '' }} </td>
-        @endforeach
-    </tr>
+    @foreach ($details as $key => $value)
+        <tr>
+            <td style="border: 1px solid #5e5b5b;text-align: left;padding: 4px;width:10%">{{ $value["ItemCode"] }} </td>
+            <td style="border: 1px solid #5e5b5b;text-align: left;padding: 4px;width:30%">{{ $value["ItemName"] }} </td>
+            <td style="border: 1px solid #5e5b5b;text-align: left;padding: 4px;width:10%">{{ $value["ItemCategory"] }} </td>
+            <td style="border: 1px solid #5e5b5b;text-align: left;padding: 4px;width:10%">{{ $value["UoMCode"] }} </td>
+            <td style="border: 1px solid #5e5b5b;text-align: left;padding: 4px;width:10%">{{ $value["ReqQty"] }} </td>
+            <td style="border: 1px solid #5e5b5b;text-align: left;padding: 4px;width:10%">{{ $value["ReqDate"] }} </td>
+            <td style="border: 1px solid #5e5b5b;text-align: left;padding: 4px;width:10%">{{ (array_key_exists('ReqNotes', $value)) ? $value["ReqNotes"] : '' }} </td>
+        </tr>
+    @endforeach
 </table>
